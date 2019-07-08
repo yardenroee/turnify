36c3162e6446e4e1fa60b2fb032d3b2964d6e7da
yardenroee <yardenroee@gmail.com>
2019-07-08 11:59:51 -0400

    playlist util + actions

---
+++ frontend/actions/playlist_actions.js
@@ -0,0 +1,51 @@
+import * as APIUtil from '../util/playlist_api_util';
+
+export const RECEIVE_ALL_PLAYLISTS = 'RECEIVE_ALL_PLAYLISTS';
+export const RECEIVE_PLAYLIST = 'RECEIVE_PLAYLIST';
+export const REMOVE_PLAYLIST = 'REMOVE_PLAYLIST';
+
+
+export const receiveAllPlaylists = (playlists) => {
+    return {
+        type: RECEIVE_ALL_PLAYLISTS,
+        playlists,
+    }
+}
+
+export const receivePlaylist = (data) => {
+
+    return {
+        type: RECEIVE_PLAYLIST,
+        data,
+    }
+}
+
+export const removePlaylist = (playlistId) => {
+
+    return {
+        type: REMOVE_PLAYLIST,
+        playlistId,
+    }
+}
+
+
+export const fetchPlaylists = () => dispatch => {
+    return APIUtil.fetchPlaylists().then(playlists => dispatch(receiveAllPlaylists(playlists)))
+}
+
+export const fetchPlaylist = (id) => dispatch => {
+    return APIUtil.fetchPlaylist(id).then(playlist => dispatch(receivePlaylist(playlist)))
+}
+
+export const createPlaylist = (playlist, user_id) => dispatch => {
+    return APIUtil.createPlaylist(playlist, user_id).then(playlist => dispatch(receivePlaylist(playlist)),
+        err => (
+            dispatch(receiveErrors(err.responseJSON))
+        ));
+};
+
+
+export const deletePlaylist = (id) => dispatch => {
+
+    return APIUtil.destroyPlaylist(id).then(() => dispatch(removePlaylist(id)));
+}
---
+++ frontend/util/playlist_api_util.js
@@ -0,0 +1,32 @@
+
+//all playlists
+export const fetchPlaylists = () => {
+    return $.ajax({
+        method: 'GET',
+        url: 'api/playlists'
+    })
+}
+
+//single playlist
+export const fetchPlaylist = (id) => {
+
+    return $.ajax({
+        method: 'GET',
+        url: `api/playlists/${id}`
+    })
+}
+
+export const createPlaylist = (playlist, user_id) => {
+    return $.ajax({
+        method: 'POST',
+        url: "api/playlists",
+        data: { playlist, user_id }
+    })
+}
+
+export const destroyPlaylist = (id) => {
+    return $.ajax({
+        method: 'DELETE',
+        url: `api/playlists/${id}`
+    })
+}
