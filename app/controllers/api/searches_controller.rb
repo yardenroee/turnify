25aff636d77c2e286cf6581f22a4322a20b78718
yardenroee <yardenroee@gmail.com>
2019-07-16 14:39:00 -0400

    searching for song now shows album and playlist it belongs to

--- app/controllers/api/searches_controller.rb
+++ app/controllers/api/searches_controller.rb
@@ -10,11 +10,16 @@ class Api::SearchesController < ApplicationController
             # .where("UPPER(artists.name) LIKE UPPER(:query) OR UPPER(albums.title) LIKE UPPER(:query) OR UPPER(songs.title) LIKE UPPER(:query)", query: str)
             @artists = @artists.uniq
 
-            @albums = Album.where("UPPER(albums.title) LIKE UPPER(:query)", query: str)
+            @albums = Album.joins("JOIN songs ON songs.album_id = albums.id")
+            .where("UPPER(albums.title) LIKE UPPER(:query) OR UPPER(songs.title) LIKE UPPER(:query)", query: str)
             @albums = @albums.uniq
 
-            @playlists = Playlist.where("UPPER(playlists.title) LIKE UPPER(:query)", query: str)
+            @playlists = Playlist.joins("JOIN playlist_songs ON playlist_songs.playlist_id = playlists.id")
+            .joins("JOIN songs ON playlist_songs.song_id = songs.id")
+            .where("UPPER(playlists.title) LIKE UPPER(:query) OR UPPER(songs.title) LIKE UPPER(:query)", query: str)
             @playlists.uniq
+
+
         end
         render :show
     end
